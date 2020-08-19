786f4808cab8d1ae83704d5badcfb09f810cca02
Lenny Linux <tippexs91@googlemail.com>
2020-08-19 19:37:15 +0200

    Updated UNIT and refactor run-local script to run npm update

--- dev/run-local.sh
+++ dev/run-local.sh
@@ -3,7 +3,9 @@ set -ex
 case $1 in
 "dev")
   echo "Building local image and mounting codebase..."
-  docker build --no-cache -t tippexs/wwp:latest .
+  docker run -it --rm -v $(pwd)/../api:/data/ tippexs/nginxspace:1.19 /bin/bash -c "cd /data/ && npm update"
+  chmod -R 777 ../wordpress ../api
+  docker build --no-cache -t tippexs/nginxspace:latest .
   docker-compose up
   ;;
 "no-build")
